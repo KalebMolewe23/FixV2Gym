fa52f40eb29325403e37297f152392a86bd52bc9
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2025-09-17 23:33:38 +0700

    update slow cms di homecontroller

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,15 +5,12 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Cms;
+use App\Models\Packets;
 
 class HomeController extends Controller
 {
     public function index(){
-
-        $daily = DB::table('packets')->select('price')->where('id', 1)->first();
-        $montly = DB::table('packets')->select('price')->where('id', 2)->first();
-        $trainer = DB::table('packet_trainer')->select('price')->where('id', 1)->first();
-        $review = DB::table('member_review')->join('member_information', 'member_information.id', '=', 'member_review.iduser')->where('member_review.status', 'Verified')->get();
 
         return view('home', [
             'daily' => $daily,
