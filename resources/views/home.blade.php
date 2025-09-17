5421584e5cbe14f430781c3093a827691df9a23e
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2025-09-17 23:31:02 +0700

    tampilan lama home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
       rel="stylesheet"
     />
     <link rel="stylesheet" href="{{ asset('assets/frontend/styles.css') }}" />
-    <title>Home Page | {{ @$content['company']['company_name'] }}</title>
+    <title>Web Design Mastery | Fitclub</title>
 
     <style>
       .logo-container {
@@ -31,8 +31,8 @@
     <nav>
       <div class="nav__logo">
       <a href="#" class="logo-container">
-        <img src="{{ asset('uploads/company_logo.png') }}" alt="logo" style="width:25%;" />
-        <span style="color:white"><strong>{{ @$content['company']['company_name'] }}</strong></span>
+        <img src="{{ asset('assets/images/logo_gym.png') }}" alt="logo" style="width:25%;" />
+        <span style="color:white"><strong>ELITE FITNESS</strong></span>
       </a>
       </div>
       <ul class="nav__links" id="menu-nav">
@@ -43,27 +43,22 @@
         <li class="link" id="menu-community"><a href="#community">Community</a></li>
     </ul>
 
-      <a href="https://forms.gle/VgTLn9x4R62NCUKa7">
-        <button class="btn" style="color:black">Join Now</button>
-    </a>
-    <a href="{{ route('login') }}">
-        <button class="btn" style="color:black">login</button>
-    </a>
+      <button class="btn" style="color:black">Join Now</button>
     </nav>
 
     <header class="section__container header__container">
       <div class="header__content">
         <span class="bg__blur"></span>
         <span class="bg__blur header__blur"></span>
-        <h4>{{ @$content['hero']['hero_mini_text'] }}</h4>
-        <h1>{{ @$content['hero']['hero_description'] }}</h1>
+        <h4>BEST FITNESS IN THE TOWN</h4>
+        <h1><span>MAKE</span> YOUR BODY SHAPE</h1>
         <p>
-          {{ @$content['hero']['hero_short_description'] }}
+            Bebaskan potensi Anda dan mulailah bentuk tubuh yang lebih kuat, lebih bugar, dan lebih percaya diri. Daftar 'Bentuk Tubuh Anda' sekarang dan saksikan transformasi luar biasa yang dapat dilakukan oleh tubuh Anda!
         </p>
         <!-- <button class="btn">Get Started</button> -->
       </div>
       <div class="header__image">
-        <img src="{{ asset('uploads/hero_image.png') }}" alt="header" />
+        <img src="{{ asset('assets/frontend/assets/header.png') }}" alt="header" />
       </div>
     </header>
 
@@ -77,32 +72,32 @@
       </div>
       <div class="explore__grid">
         <div class="explore__card">
-          <span><i class="{{ @$content['jelajahi']['program1_icon'] }} style="color:black"></i></span>
-          <h4>{{ @$content['jelajahi']['program1_title'] }}</h4>
+          <span><i class="ri-boxing-fill" style="color:black"></i></span>
+          <h4>Strength</h4>
           <p>
-            {{ @$content['jelajahi']['program1_description'] }}
+            Rangkullah esensi kekuatan saat kita menyelami berbagai dimensinya secara fisik, mental, dan emosional.
           </p>
           <!-- <a href="#">Join Now <i class="ri-arrow-right-line"></i></a> -->
         </div>
         <div class="explore__card">
-          <span><i class="{{ @$content['jelajahi']['program2_icon'] }}" style="color:black"></i></span>
-          <h4>{{ @$content['jelajahi']['program2_title'] }}</h4>
+          <span><i class="ri-heart-pulse-fill" style="color:black"></i></span>
+          <h4>Physical Fitness</h4>
           <p>
-            {{ @$content['jelajahi']['program2_description'] }}
+            Mencakup berbagai aktivitas untuk meningkatkan kesehatan, kekuatan, fleksibilitas, dan kesejahteraan secara keseluruhan.
           </p>
         </div>
         <div class="explore__card">
-          <span><i class="{{ @$content['jelajahi']['program3_icon'] }} style="color:black"></i></span>
-          <h4>{{ @$content['jelajahi']['program3_title'] }}</h4>
+          <span><i class="ri-run-line" style="color:black"></i></span>
+          <h4>Fat Lose</h4>
           <p>
-            {{ @$content['jelajahi']['program3_description'] }}
+            Melalui kombinasi rutinitas latihan dan bimbingan ahli, kami akan memberdayakan Anda untuk mencapai tujuan Anda.
           </p>
         </div>
         <div class="explore__card">
-          <span><i class="{{ @$content['jelajahi']['program4_icon'] }}" style="color:black"></i></span>
-          <h4>{{ @$content['jelajahi']['program4_title'] }}</h4>
+          <span><i class="ri-shopping-basket-fill" style="color:black"></i></span>
+          <h4>Weight Gain</h4>
           <p>
-            {{ @$content['jelajahi']['program4_description'] }}
+            Dirancang untuk individu, program kami menawarkan pendekatan yang efektif untuk menambah berat badan secara berkelanjutan.
           </p>
         </div>
       </div>
@@ -111,45 +106,45 @@
     <section id="service" class="section__container class__container">
       <div class="class__image">
         <span class="bg__blur"></span>
-        <img src="{{ asset('uploads/service_image1.png') }}" alt="class" class="class__img-1" />
-        <img src="{{ asset('uploads/service_image2.png') }}" alt="class" class="class__img-2" />
+        <img src="{{ asset('assets/frontend/assets/class-1.jpg') }}" alt="class" class="class__img-1" />
+        <img src="{{ asset('assets/frontend/assets/class-2.jpg') }}" alt="class" class="class__img-2" />
       </div>
       <div class="class__content">
-        <h2 class="section__header">{{ @$content['service']['service_title'] }}</h2>
+        <h2 class="section__header">THE CLASS YOU WILL GET HERE</h2>
         <p>
-          {{ @$content['service']['service_description'] }}
+            Dipimpin oleh tim instruktur ahli dan motivator kami, “Kelas yang Akan Anda Dapatkan di Sini” adalah sesi berenergi tinggi dan berorientasi pada hasil yang menggabungkan perpaduan sempurna antara latihan kardio, latihan kekuatan, dan latihan fungsional. Setiap kelas dirancang dengan cermat untuk membuat Anda tetap terlibat dan tertantang, memastikan Anda tidak pernah mencapai titik terendah dalam upaya kebugaran Anda.
         </p>
         <!-- <button class="btn">Book A Class</button> -->
       </div>
     </section>
 
     <section id="about" class="section__container join__container">
-      <h2 class="section__header">{{ @$content['join']['join_title'] }}</h2>
+      <h2 class="section__header">WHY JOIN US ?</h2>
       <p class="section__subheader">
-        {{ @$content['join']['join_description'] }}
+        Basis keanggotaan kami yang beragam menciptakan suasana yang bersahabat dan suportif, di mana Anda dapat menjalin pertemanan dan tetap termotivasi.
       </p>
       <div class="join__image">
-        <img src="{{ asset('uploads/join_image1.png') }}" alt="Join" />
+        <img src="{{ asset('assets/frontend/assets/join.jpg') }}" alt="Join" />
         <div class="join__grid">
           <div class="join__card">
-            <span><i class="{{ @$content['join']['why_join_title1'] }}" style="color:black"></i></span>
+            <span><i class="ri-user-star-fill" style="color:black"></i></span>
             <div class="join__card__content">
-              <h4>{{ @$content['join']['why_join_description1'] }}</h4>
-              <p>{{ @$content['join']['why_join_long_description1'] }}</p>
+              <h4>Personal Trainer</h4>
+              <p>Buka potensi Anda dengan Personal Trainer ahli kami.</p>
             </div>
           </div>
           <div class="join__card">
-            <span><i class="{{ @$content['join']['why_join_title2'] }}" style="color:black"></i></span>
+            <span><i class="ri-vidicon-fill" style="color:black"></i></span>
             <div class="join__card__content">
-              <h4>{{ @$content['join']['why_join_description2'] }}</h4>
-              <p>{{ @$content['join']['why_join_long_description2'] }}</p>
+              <h4>Practice Sessions</h4>
+              <p>Tingkatkan kebugaran Anda dengan sesi latihan.</p>
             </div>
           </div>
           <div class="join__card">
-            <span><i class="{{ @$content['join']['why_join_title3'] }}" style="color:black"></i></span>
+            <span><i class="ri-building-line" style="color:black"></i></span>
             <div class="join__card__content">
-              <h4>{{ @$content['join']['why_join_description3'] }}</h4>
-              <p>{{ @$content['join']['why_join_long_description3'] }}</p>
+              <h4>Good Management</h4>
+              <p>Manajemen yang mendukung, untuk kesuksesan kebugaran Anda.</p>
             </div>
           </div>
         </div>
@@ -162,36 +157,97 @@
         Berikut paket harga kami dengan berbagai tingkatan sesuai kebutuhan Anda, masing-masing disesuaikan untuk memenuhi preferensi dan aspirasi kebugaran yang berbeda.
       </p>
       <div class="price__grid">
-        @foreach ($package as $item)
-          <div class="price__card">
-            <div class="price__card__content">
-              <h4>{{ $item->packet_name }}</h4>
-              <h3>
-                @foreach ($package as $item)
-                    @if ($item->promote != 0)
-                        <span style="text-decoration: line-through; color: red; font-size: 20px">
-                            Rp{{ number_format((float) $item->price, 0, ',', '.') }}
-                        </span>
-                        &nbsp;
-                        <span style="font-size:30px;">
-                            Rp{{ number_format((float) $item->promote, 0, ',', '.') }}
-                        </span>
-                    @else
-                        Rp{{ number_format((float) $item->price, 0, ',', '.') }}
-                    @endif
-                @endforeach
-
-              </h3>
-              @if ($item->description)
-                  @foreach ($item->description as $itemdesc)    
-                    <p>
-                      <i class="ri-checkbox-circle-line"></i>
-                      {{ $itemdesc }}
-                    </p>
-                  @endforeach
-              @endif
-            </div>
+        <div class="price__card">
+          <div class="price__card__content">
+            <h4>Paket Harian</h4>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              Smart workout plan
+            </p>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              At home workouts
+            </p>
           </div>
+        </div>
+        <div class="price__card">
+          <div class="price__card__content">
+            <h4>Paket Bulanan</h4>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              PRO Gyms
+            </p>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              Smart workout plan
+            </p>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              At home workouts
+            </p>
+          </div>
+        </div>
+        <div class="price__card">
+          <div class="price__card__content">
+            <h4>Paket Trainer</h4>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              ELITE Gyms & Classes
+            </p>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              PRO Gyms
+            </p>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              Smart workout plan
+            </p>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              At home workouts
+            </p>
+            <p>
+              <i class="ri-checkbox-circle-line"></i>
+              Personal Training
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="community" class="review">
+      <div class="section__container review__container">
+        <span><i class="ri-double-quotes-r"></i></span>
+        @foreach($review as $index => $v_review)
+            <div class="review__content" data-index="{{ $index }}" style="{{ $index === 0 ? '' : 'display: none;' }}">
+                <h4>MEMBER REVIEW</h4>
+                <p>
+                    {{ $v_review->description }}
+                </p>
+                <div class="review__rating">
+                    @for($i = 1; $i <= 5; $i++)
+                        @if($i <= floor($v_review->start))
+                            <span><i class="ri-star-fill"></i></span> <!-- Full Star -->
+                        @elseif($i - $v_review->start < 1)
+                            <span><i class="ri-star-half-fill"></i></span> <!-- Half Star -->
+                        @else
+                            <span><i class="ri-star-line"></i></span> <!-- Empty Star -->
+                        @endif
+                    @endfor
+                </div>
+                <div class="review__footer">
+                    <div class="review__member">
+                        <img src="{{ asset('storage/' . $v_review->photo) }}" alt="member">
+                        <div class="review__member__details">
+                            <h4>{{ $v_review->name }}</h4>
+                        </div>
+                    </div>
+                    <div class="review__nav">
+                        <span class="nav-left"><i class="ri-arrow-left-line"></i></span>
+                        <span class="nav-right"><i class="ri-arrow-right-line"></i></span>
+                    </div>
+                </div>
+            </div>
         @endforeach
       </div>
     </section>
@@ -202,17 +258,18 @@
       <div class="footer__col">
         <div class="footer__logo">
           <a href="#" class="logo-container">
-            <img src="{{ asset('uploads/company_logo.png') }}" alt="logo" style="width:25%;" />
-            <span style="color:white"><strong>{{ @$content['company']['company_name'] }}</strong></span>
+            <img src="{{ asset('assets/images/logo_gym.png') }}" alt="logo" style="width:25%;" />
+            <span style="color:white"><strong>ELITE FITNESS</strong></span>
           </a>
         </div>
         <p>
-          {{ @$content['about']['about_description'] }}
+          Take the first step towards a healthier, stronger you with our
+          unbeatable pricing plans. Let's sweat, achieve, and conquer together!
         </p>
         <div class="footer__socials">
-          <a href="{{ @$content['about']['about_sosmed_link1'] }}"><i class="{{ @$content['about']['about_sosmed_icon1'] }}"></i></a>
-          <a href="{{ @$content['about']['about_sosmed_link2'] }}"><i class="{{ @$content['about']['about_sosmed_icon2'] }}"></i></a>
-          <a href="{{ @$content['about']['about_sosmed_link3'] }}"><i class="{{ @$content['about']['about_sosmed_icon3'] }}"></i></a>
+          <a href="#"><i class="ri-facebook-fill"></i></a>
+          <a href="#"><i class="ri-instagram-line"></i></a>
+          <a href="#"><i class="ri-twitter-fill"></i></a>
         </div>
       </div>
       <div class="footer__col">
@@ -237,7 +294,7 @@
       </div>
     </footer>
     <div class="footer__bar">
-      Copyright © {{ date('Y') }} - {{ @$content['company']['company_name'] }}. All rights reserved.
+      Copyright © 2023 Web Design Mastery. All rights reserved.
     </div>
     <script src="{{ asset('assets/frontend/script.js') }}"></script>
     <script>
